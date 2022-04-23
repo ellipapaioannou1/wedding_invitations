5d721230c048117362dd7a986c1aec0416a88c48
Elli <papaioannou.elli@gmail.com>
2022-04-23 12:28:49 +0300

    Added new background photo

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,7 +1,8 @@
 class PasswordsController < ApplicationController
     # allows only logged in users
     before_action :require_user_logged_in!
-    def edit; end
+    def edit; 
+    end
     def update
       # update user password
       if Current.user.update(password_params)
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,9 @@
 class SessionsController < ApplicationController
-    def new; end
+    def new; 
+    end
     def create
       user = User.find_by(email: params[:email])
-      # finds existing user, checks to see if user can be authenticated
       if user.present? && user.authenticate(params[:password])
-      # sets up user.id sessions
         session[:user_id] = user.id
         redirect_to root_path, notice: 'Logged in successfully'
       else
@@ -13,7 +12,6 @@ class SessionsController < ApplicationController
       end
     end
     def destroy
-      # deletes user session
       session[:user_id] = nil
       redirect_to root_path, notice: 'Logged Out'
     end
